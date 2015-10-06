90a4ad74e5f8662a08c236cb92227011d8842df1
HiyamaKun <frederic.nault@gmail.com>
2015-10-05 21:34:41 -0400

    Update Licence Update

--- Index.php
+++ Index.php
@@ -1,4 +1,23 @@
 <?php
+/*
+    The MIT License (MIT)
+    Copyright (c) 2015 Frédéric Nault
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+ */
 	require_once 'Core/Start.php';
 	#var_dump(_DataSet('localhost', 'root', '', 'TheOne', 'Users', 'id:null,first_name:Patate,last_name:Pilées,password:Menoume,localisation:Montréal,username:Test,email:test@test.com'));
 	#var_dump(_DatasGet('localhost', 'root', '', 'TheOne', 'Users', 'last_name'));
--- README.md
+++ README.md
@@ -1,15 +1,6 @@
-# E-CMS - Description des Fontionnalitées #
+# TheOne - Description des Fontionnalitées #
 
 ----------
 
 ## Fonctionnalitées :##
 ### Version 0.0.1 : ###
- - Fonctions de connexion pour les utilisateurs existants
- - Fonctions d'inscription pour les nouveaux utilisateurs
- - Fonctions de rangs ( Permissions ) pour certains groupes d'utilisateurs
- - Fonctions de page de profile pour chaque utilisateur
-	 - Fonctions d'upload d'images sur le profil de l'utilisateur
-	 - Fonctions de modification des information du profile 
- - Fonctions de publication de contenus
-	 - Fonctions d'ajout de commentaires au différents contenus publiés
- - Fonctions 
